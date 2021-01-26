c1daf5a77aa7d536e2f5e1b984c7587f14e0ed9d
gabriel <soadhenri@gmail.com>
2021-01-25 22:18:01 -0300

    created an authentification middleware

--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,14 @@
 import { Router } from 'express';
 
+import authMiddleware from './app/middlewares/auth';
+
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
 
 const routes = new Router();
 
 routes.post('/users', UserController.store);
+routes.put('/users', authMiddleware, UserController.update);
 
 routes.post('/sessions', SessionController.store);
 
